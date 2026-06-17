09d9bb25f06ff799dbf80a7875c36b234689f5b1
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-06-17 09:26:27 -0300

    Alteracoes importantes em l031q07.js

--- l031q07.js
+++ l031q07.js
@@ -3,4 +3,14 @@
 let num1, num2, num3, media;
 
 console.log(" Digite o primeiro número: ");
-num1 = 5
+num1 = 5;
+
+console.log(" Digite o primeiro número: ");
+num2 = 5;
+
+console.log(" Digite o primeiro número: ");
+num3 = 5;
+
+media = (num1 + num2 + num3) / 3;
+
+console.log(" A media é: ", media);
