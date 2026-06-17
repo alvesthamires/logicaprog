b01613ed0bd6c15fe27dc389eb9bccf9a0f22b88
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-06-17 11:08:16 -0300

    Exemplo com else if

---
+++ l041q09.js
@@ -0,0 +1,15 @@
+
+
+let num;
+ console.log("Digite um número: ");
+ num = -5;
+ console.log(`Número informado: ${num}`);
+
+ if( num > 0){
+    console.log(`${num} é positivo.`);
+
+ } else if (num < 0){
+    console.log(`${num} é negativo.`);
+ } else{
+    console.log(`${num} é mulo`);
+ }
