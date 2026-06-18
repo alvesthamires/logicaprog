9820d13919e5c8647b0521be34ed1e3c80ca18ac
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-06-18 10:12:55 -0300

    Estrutura de repeticao while

---
+++ l051q03.js
@@ -0,0 +1,12 @@
+//Desenvolver um programa que apresente os quadrados dos números inteiros de 15 a 200.
+
+let cont = 15;
+let pot;
+
+
+while(cont <= 200){
+    pot = cont * cont;
+    console.log(`${cont} X ${cont} =  ${pot}`);
+    cont++;
+   
+}
