ba1900931e2b3c09c53324009b70c0aa3de172c8
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-06-22 09:28:52 -0300

    Exemplo com switch case

---
+++ l061q05.js
@@ -0,0 +1,79 @@
+let mes;
+
+
+console.log(" 1 - Janeiro");
+console.log(" 2 - Fevereiro");
+console.log(" 3 - Março");
+console.log(" 4 - Abril");
+console.log(" 5 - Maio");
+console.log(" 6 - Junho");
+console.log(" 7 - Julho" );
+console.log(" 8 - Agosto");
+console.log(" 9 - Setembro");
+console.log(" 10 - Outubro");
+console.log(" 11 - Novembro");
+console.log(" 12 - Dezembro\n");
+
+console.log(" Digite um número (correspondente a um mês): ");
+mes = 10;
+console.log(`O número digitado foi ${mes}\n`);
+
+
+switch ( mes ){
+
+    case 1:
+        console.log("Você selecionou: 1 - Janeiro");
+    break;
+
+        case 2:
+            console.log("Você selecionou: 2 - Fevereiro");
+            break;
+
+           case 3:
+            console.log("Você selecionou: 3 - Março");
+            break;
+
+            case 4: 
+            console.log("Você selecionou: 4 - Abril");
+            break;
+
+             case 5:
+            console.log("Você selecionou: 5 - Maio");
+            break;
+
+           case 6:
+            console.log("Você selecionou: 6 - Junho");
+            break;
+
+            case 7:
+            console.log("Você selecionou: 7 - Julho");
+            break;
+
+           case 8:
+            console.log("Você selecionou: 8 - Agosto");
+            break;
+
+           case 9:
+            console.log("Você selecionou: 9 - Setembro");
+            break;
+
+           case 10:
+            console.log("Você selecionou: 10 - Outubro");
+            break;
+
+             case 11:
+            console.log("Você selecionou: 11 - Novembro");
+            break;
+
+           case 12:
+            console.log("Você selecionou: 12 - Dezembro");
+            break;
+
+             default: 
+         console.log("ERRO: Você não selecionou uma opção válida.");
+        break;
+
+
+
+          
+}
