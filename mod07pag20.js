d275bad89a4fb730163f0367bf8ccaf5bfef87dd
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-06-26 10:05:05 -0300

    Correcao relevante

--- mod07pag20.js
+++ mod07pag20.js
@@ -4,13 +4,13 @@ let produtos = [
     {nome: "Tablet", preco: 3500}
 ];
 
-console.log(produtos[0]);
-console.log(produtos[2]);
+console.log(produtos[0].nome);
+console.log(produtos[2].nome);
 console.log("\n");
 
 
 for(let produto of produtos){
-    console.log(produto);
+    console.log(`Produto: ${produto.nome} | Preço: R$ ${produto.preco.toFixed}`);
 }
 
 produtos[0].preco = 3800;
